5c0c2de899d87b357d10110d62b593e50460c5e3
ademidun <tomiademidun@gmail.com>
2022-03-22 07:56:13 -0400

    add atlas content index and content parser

--- README.md
+++ README.md
@@ -7,10 +7,31 @@ Atlas is a tool that allows you to parse a collection of URLs, index them into d
 
 `source install.sh`
 
+Put your environment variables into the newly created .env file
+
+`source .env`
+
 ## Quickstart
 
-To use the API:
 
 ```shell
+# 1. Parse and index your content:
+python atlas/content_parser.py
+
+# 2. Initialize your content:
+python atlas/content_index.py
+
+# 3. Run the API
 python api/api.py
+
+# 4. Send a GET request to your api
+curl --location --request GET 'http://127.0.0.1:8080/api/search?q=what+is+an+NFT'
+# or open your browser to `http://127.0.0.1:8080/api/search?q=<your_search_term>` 
 ```
+
+
+## Troubleshooting
+
+`ModuleNotFoundError: No module named 'atlas'`
+
+Set your $PYTHONPATH. See this [SO answer](ModuleNotFoundError: No module named 'atlas')
--- api/api.py
+++ api/api.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+from atlas.content_index import ContentIndex
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +10,21 @@ CORS(app)
 @app.route("/")
 def hello():
     return "Hello world"
+
+
+@app.route("/api/search", methods=['GET'])
+def search():
+    query = request.args.get('q')
+    show_raw_results = request.args.get('raw')
+    if not query:
+        return jsonify({'error': f"Please enter a search term in your URL: ?q=<search_term>"}), 400
+
+    elif len(query) < 3:
+        return jsonify({'error': f"Please enter at least 3 characters in your search term"}), 400
+
+    content_index = ContentIndex()
+    results = content_index.search(query, show_raw_results=show_raw_results)
+    return jsonify({'results': results})
 
 
 if __name__ == "__main__":
